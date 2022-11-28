c559e8536697ac79331a30755d50255a8a7147bd
ParkJunHa <jh01love@naver.com>
2022-11-28 20:00:20 +0900

    [Gold I] Title: 서로소, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/4355. 서로소/서로소.py
@@ -0,0 +1,14 @@
+n = int(input())
+while(n):
+    result = n
+    for i in range(2, round(n ** 0.5) + 1):
+        if n % i == 0:
+            while n % i == 0:
+                n //= i
+            result *= 1 - (1 / i)
+                
+    if n > 1:
+        result *= 1 - (1 / n)
+    
+    print(round(result))
+    n = int(input())
