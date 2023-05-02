1f446cdd5df7ec891268faabfbbfdfa67a526986
ParkJunHa <jh01love@naver.com>
2023-05-02 20:11:45 +0900

    [Silver V] Title: CD, Time: 2204 ms, Memory: 239764 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.py
+++ 백준/Silver/4158. CD/CD.py
@@ -1,17 +1,6 @@
 import sys
 input = sys.stdin.readline
-print = sys.stdout.write
-def bi_search(lst, start, end, target): # start와 end는 인덱스
-    while start <= end:
-        mid = (start + end) // 2
-        if lst[mid] == target:
-            return 1
-        elif lst[mid] < target:
-            start  = mid + 1
-        else:
-
-            end = mid - 1
-    return 0
+# print = sys.stdout.write
 
 while True:
     n, m = map(int, input().split())
@@ -26,8 +15,4 @@ while True:
     for _ in range(m):
         m_list.append(int(input()))
 
-    result = 0
-    for x in n_list:
-        start, end = 0, len(m_list)
-        result += bi_search(m_list, start, end, x)
-    print(str(result)+"\n")
+    print(len(set(n_list) & set(m_list)))
