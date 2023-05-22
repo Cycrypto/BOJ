dcc783b8808e897a68996c13545bc6862ff025a0
ParkJunHa <jh01love@naver.com>
2023-05-22 22:52:03 +0900

    Update 이진 탐색 트리.py

--- 백준/Platinum/2957. 이진 탐색 트리/이진 탐색 트리.py
+++ 백준/Platinum/2957. 이진 탐색 트리/이진 탐색 트리.py
@@ -1,40 +1,26 @@
+from bisect import bisect_left, insort_left
 import sys
 input = sys.stdin.readline
+print = sys.stdout.write
+print('0\n')
+tc = int(input())
+M = dict()
+M[int(input())] = 0
+cnt = 0
 
-def update(i):
-  check[i] = 1
-  while i<=M:
-    fen[i] += 1
-    i += i&-i
+for i in range(tc - 1):
+    k = list(set(M.keys()))
+    val = int(input())
+    lower = bisect_left(k, val)
+    cmp1, cmp2 = 0, 0
+    
+    if lower < len(k):
+        cmp1 = M[k[lower]]
+    
+    if 0 < lower:
+        lower -= 1
+        cmp2 = M[k[lower]]
 
-def cal(i):
-  S = 0
-  while i:
-    S += fen[i]
-    i -= i&-i
-  return S
-
-def find(x,i,c):
-  s = cal(i)
-  if s==x and check[i]:
-    return i
-  if s<x:
-    return find(x,i+(1<<c),c-1)
-  return find(x,i-(1<<c),c-1)
-
-N = int(input()); M = 1<<19
-fen,check,depth = [[0]*(M+1) for i in range(3)]
-C = 0
-for _ in range(N):
-  x = int(input()); s = cal(x)
-  if fen[M]==0:
-    pass
-  elif s==0:
-    depth[x] = depth[find(1,M,18)]+1
-  elif s==fen[M]:
-    depth[x] = depth[find(s,M,18)]+1
-  else:
-    depth[x] = max(depth[find(s,M,18)],depth[find(s+1,M,18)])+1
-  C += depth[x]
-  update(x)
-  print(C)
+    M[val] = max(cmp1, cmp2) + 1
+    cnt += max(cmp1, cmp2) + 1
+    print(str(cnt)+"\n")
