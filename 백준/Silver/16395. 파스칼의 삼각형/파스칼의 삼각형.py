5780eb9b40dd6b5fe4e974633e73c25c6e288d7b
ParkJunHa <jh01love@naver.com>
2023-06-04 17:08:44 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.py
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -1,7 +1,19 @@
+import sys
+sys.setrecursionlimit(int(1e9))
 N, M = map(int,input().split())
-pascal = [[1 for _ in range(N)] for _ in range(N)]
+mem = [[0] * N for _ in range(N)]
 
-for i in range(1, N):
-    for j in range(1, i):
-        pascal[i][j] = pascal[i-1][j] + pascal[i-1][j-1]
-print(pascal[N-1][M-1])
+def pascal(n, m):
+    if n == m or m == 0:
+        return 1
+    
+    if m >= n:
+        return 0
+    
+    if mem[n][m] != 0:
+        return mem[n][m]
+    else:
+        mem[n][m] = pascal(n-1, m) + pascal(n-1, m-1)
+        return mem[n][m]
+
+print(pascal(N - 1, M - 1))
