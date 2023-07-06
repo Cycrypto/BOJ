f4e0306e6294d0a83e4f556d87ac6f9adcce0d56
ParkJunHa <jh01love@naver.com>
2023-07-06 22:29:54 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 44 ms, Memory: 31416 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,5 +1,13 @@
-s = [0, 1, 3]
-for i in range(3, 1001):
-  s.append((s[i - 2] * 2) + s[i - 1])
-n = int(input())
-print(s[n] % 10007)
+def tiling(n):
+    if n == 0:
+        return 1
+    
+    if dp[n] != 0:
+        return dp[n]
+    
+    dp[n] = (tiling(n-1) + tiling(n-2)*2) % 10007
+    return dp[n]
+
+dp = [0] * 1001
+dp[1], dp[2] = 1, 3
+print(tiling(int(input())))
