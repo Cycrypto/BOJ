ec9ba95c047efe1adf76f5f29e5bf4818bb91d71
ParkJunHa <jh01love@naver.com>
2023-09-16 09:55:32 +0900

    [Gold V] Title: 강의실 배정, Time: 400 ms, Memory: 73584 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,20 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+timeline = list(list(map(int, input().split())) for _ in range(n))
+timeline.sort()
+
+pq = []
+heapq.heappush(pq, timeline[0][1])
+
+for i in range(1, n):
+    if timeline[i][0] < pq[0]:
+        heapq.heappush(pq, timeline[i][1])
+    
+    else:
+        heapq.heappop(pq)
+        heapq.heappush(pq, timeline[i][1])
+
+print(len(pq))
