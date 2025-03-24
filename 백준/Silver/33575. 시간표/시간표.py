aa52b9b2d71a6c2499f116cb394b40961fa0f438
ParkJunHa <jh01love00@gmail.com>
2025-03-24 16:24:48 +0900

    [Silver IV] Title: 시간표, Time: 116 ms, Memory: 52500 KB -BaekjoonHub

--- 백준/Silver/33575. 시간표/시간표.py
+++ 백준/Silver/33575. 시간표/시간표.py
@@ -1,34 +1,24 @@
-from itertools import groupby
 import sys
+from itertools import groupby
 
 input = sys.stdin.readline
 
-N,M,A,B = map(int, input().split())
+N, M, A, B = map(int, input().split())
 T = list(map(int, input().split()))
 like = set(map(int, input().split()))
 hate = set(map(int, input().split()))
 
+mapped = [
+    True if x in like else False if x in hate else None
+    for x in T
+]
+
 score = 0
-prev = None
-length = 0
 
-for t in T:
-    if t in like:
-        cur = True
-    elif t in hate:
-        cur = False
-    else:
-        cur = None
-    
-    if cur == prev and cur is not None:
-        length += 1
-    else:
-        if prev is not None and length >= 3:
-            score += length if prev else -length
-        length = 1 if cur is not None else 0
-        prev = cur
-
-if prev is not None and length >= 3:
-    score += length if prev else -length
+for key, group in groupby(mapped):
+    if key == True or key == False:
+        count = sum(1 for _ in group)
+        if count >= 3:
+            score += count if key else -count
 
 print(score)
