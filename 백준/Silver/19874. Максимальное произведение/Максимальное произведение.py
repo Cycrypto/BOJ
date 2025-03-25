eb570a3ef88bce52d1651ace5786edc98a0eb347
ParkJunHa <jh01love00@gmail.com>
2025-03-25 19:26:28 +0900

    [Silver IV] Title: Максимальное произведение, Time: 176 ms, Memory: 56452 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/19874. Максимальное произведение/Максимальное произведение.py
+++ 백준/Silver/19874. Максимальное произведение/Максимальное произведение.py
@@ -1,9 +1,13 @@
-int(input())
+import itertools
+
+n = int(input())
 l = list(map(int, input().split()))
+s = list(itertools.accumulate(l))
+
 result = -1
 ans = 0
-for i in range(len(l)):
-    if (k:=sum(l[:i+1]) * sum(l[i+1:])) > result:
+for i in range(n-1):
+    if (k:=s[i] * (s[-1] - s[i])) > result:
         result = k
         ans = i + 1
 print(ans)
