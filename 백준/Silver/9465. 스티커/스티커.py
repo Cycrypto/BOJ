f1bb64d1db4632a898bbdce27e9af9ab5be94269
ParkJunHa <jh01love00@gmail.com>
2026-02-02 15:01:36 +0900

    [Silver I] Title: 스티커, Time: 384 ms, Memory: 41012 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,23 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-tc = int(input().strip())
+tc = int(input())
 for _ in range(tc):
-    k = int(input().strip())
-    s = [list(map(int, input().split())) for _ in range(2)]
+    n = int(input())
+    s0 = list(map(int, input().split()))
+    s1 = list(map(int, input().split()))
 
-    if k == 1:
-        print(max(s[0][0], s[1][0]))
+    if n == 1:
+        print(max(s0[0], s1[0]))
         continue
 
-    dp = [[0] * k for _ in range(2)]
-    dp[0][0] = s[0][0]
-    dp[1][0] = s[1][0]
-    dp[0][1] = s[0][1] + dp[1][0]
-    dp[1][1] = s[1][1] + dp[0][0]
+    prev2_0 = s0[0]
+    prev2_1 = s1[0]
 
-    for j in range(2, k):
-        dp[0][j] = s[0][j] + max(dp[1][j-1], dp[1][j-2])
-        dp[1][j] = s[1][j] + max(dp[0][j-1], dp[0][j-2])
+    prev1_0 = s0[1] + prev2_1
+    prev1_1 = s1[1] + prev2_0
 
-    print(max(dp[0][k-1], dp[1][k-1]))
+    for j in range(2, n):
+        cur0 = s0[j] + max(prev1_1, prev2_1)
+        cur1 = s1[j] + max(prev1_0, prev2_0)
+
+        prev2_0, prev2_1 = prev1_0, prev1_1
+        prev1_0, prev1_1 = cur0, cur1
+
+    print(max(prev1_0, prev1_1))
