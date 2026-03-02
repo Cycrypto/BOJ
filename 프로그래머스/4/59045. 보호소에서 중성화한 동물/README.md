6f39eba9d6592f67404a7b20f4893280a6daac91
ParkJunHa <jh01love00@gmail.com>
2026-03-02 20:47:24 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/README.md
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/README.md
@@ -1,6 +1,6 @@
 # [level 4] 보호소에서 중성화한 동물 - 59045 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59045#fn1) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59045) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 2월 4일 13:8:2
+2026년 03월 02일 20:47:22
 
 ### 문제 설명
 
--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,9 +1,9 @@
 -- 코드를 입력하세요
-SELECT a.animal_id, a.animal_type, a.name
-from animal_ins a
-right join animal_outs b
-using(animal_id)
-where
-    a.sex_upon_intake != b.sex_upon_outcome
-    
-order by a.animal_id
+SELECT i.animal_id, i.animal_type, i.name
+from animal_ins i
+join animal_outs o
+on i.animal_id = o.animal_id
+where 
+    i.sex_upon_intake like "%intact%" and
+    (o.sex_upon_outcome like "%Spayed%" or
+    o.sex_upon_outcome like "%Neutered%")
