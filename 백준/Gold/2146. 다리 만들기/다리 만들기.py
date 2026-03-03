101eabca425623b55976ff51e70a2c16bf5ddb66
ParkJunHa <jh01love00@gmail.com>
2026-03-03 11:40:23 +0900

    [Gold III] Title: 다리 만들기, Time: 72 ms, Memory: 35076 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -1,70 +1,58 @@
+import sys
 from collections import deque
+
+input = sys.stdin.readline
+
 n = int(input())
-result = 999
-graph = [list(map(int, input().split())) for _ in range(n)]
-
-continent_map = [[0 for _ in range(n)] for __ in range(n)]
-
-def getContinent(x, y, mapped):
-  visited = [[0 for _ in range(n)] for __ in range(n)]
-  q = deque()
-  q.append([x, y])
-  visited[x][y] = 1
-  graph[x][y] = mapped
-  dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
-  
-  while q:
-    cx, cy = q.popleft()
-    for i in range(4):
-      nx = dx[i] + cx
-      ny = dy[i] + cy
-
-      if 0 <= nx < n and 0 <= ny < n:  # 범위안에 있을때
-        if visited[nx][ny] == 0 and graph[nx][ny] != 0:
-          graph[nx][ny] = mapped
-          visited[nx][ny] = 1
-          q.append([nx, ny])
-
-m = 2
-for i in range(n):
-  for j in range(n):
-    if graph[i][j] == 1:
-
-      getContinent(i, j, m)  # 2부터 시작하는 숫자로 매핑
-      m += 1
+g = list(list(map(int, input().split())) for _ in range(n))
+visited = [[-1] * n for _ in range(n)]
+dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
 
 
-def buildBriedge(nn):
-  global result
-  visited = [[-1 for _ in range(n)] for __ in range(n)]
-  q = deque()
-  dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
-
-  for i in range(n):
-    for j in range(n):
-      if graph[i][j] == nn:
-        q.append([i, j])
-        visited[i][j] = 0
-
-
-
-  while q:
-    cx, cy= q.popleft()
-    for i in range(4):
-      nx = dx[i] + cx
-      ny = dy[i] + cy
-      if 0 <= nx < n and 0 <= ny < n:
-        if graph[nx][ny] > 0 and graph[nx][ny] != nn:
-          result = min(result, visited[cx][cy])
-          return
-
-        if graph[nx][ny] == 0 and visited[nx][ny] == -1:
-          visited[nx][ny] = visited[cx][cy] + 1
-          q.append([nx, ny])
+def label_islands():
+    island_id = 2
+    for i in range(n):
+        for j in range(n):
+            if g[i][j] == 1:
+                q = deque([(i, j)])
+                g[i][j] = island_id
+                while q:
+                    x, y = q.popleft()
+                    for k in range(4):
+                        nx, ny = x + dx[k], y + dy[k]
+                        if 0 <= nx < n and 0 <= ny < n and g[nx][ny] == 1:
+                            g[nx][ny] = island_id
+                            q.append((nx, ny))
+                island_id += 1
 
 
+def shortest_bridge():
+    dist = [[-1] * n for _ in range(n)]
+    owner = [[0] * n for _ in range(n)]
+    q = deque()
 
-for i in range(2, m):
-  buildBriedge(i)
-  
-print(result)
+    for i in range(n):
+        for j in range(n):
+            if g[i][j] >= 2:
+                dist[i][j] = 0
+                owner[i][j] = g[i][j]
+                q.append((i, j))
+
+    ans = float('INF')
+    while q:
+        cx, cy = q.popleft()
+        for k in range(4):
+            nx, ny = cx + dx[k], cy + dy[k]
+            if 0 <= nx < n and 0 <= ny < n:
+                if dist[nx][ny] == -1:
+                    dist[nx][ny] = dist[cx][cy] + 1
+                    owner[nx][ny] = owner[cx][cy]
+                    q.append((nx, ny))
+                else:
+                    if owner[nx][ny] != owner[cx][cy]:
+                        ans = min(ans, dist[nx][ny] + dist[cx][cy])
+    return ans
+
+
+label_islands()
+print(shortest_bridge())
