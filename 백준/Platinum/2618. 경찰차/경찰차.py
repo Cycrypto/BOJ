927c9673d9fa9b04f6a74ffc741c68225e03650e
ParkJunHa <jh01love00@gmail.com>
2026-03-07 10:13:43 +0900

    [Platinum IV] Title: 경찰차, Time: 3012 ms, Memory: 71220 KB -BaekjoonHub

--- 백준/Platinum/2618. 경찰차/경찰차.py
+++ 백준/Platinum/2618. 경찰차/경찰차.py
@@ -1,66 +1,66 @@
 import sys
-sys.setrecursionlimit(int(1e8))
-MAX = 1001
-INF = 1e9+7
-dp = [[-1]* MAX for _ in range(MAX)]
-trace = []
-def get_dist(a, b):
-    # print(a, b)
-    return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
-def solve(posA, posB):
+input = sys.stdin.readline
 
-    if posA == m or posB == m:
+N = int(input())
+W = int(input())
+
+cases = [(0, 0)]
+for _ in range(W):
+    x, y = map(int, input().split())
+    cases.append((x, y))
+
+
+def dist1(a, nxt):
+    if a == 0:
+        x1, y1 = 1, 1
+    else:
+        x1, y1 = cases[a]
+    x2, y2 = cases[nxt]
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def dist2(b, nxt):
+    if b == 0:
+        x1, y1 = N, N
+    else:
+        x1, y1 = cases[b]
+    x2, y2 = cases[nxt]
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+sys.setrecursionlimit(10 ** 6)
+dp = [[-1] * (W + 1) for _ in range(W + 1)]
+
+
+def solve(a, b):
+    nxt = max(a, b) + 1
+    if nxt > W:
         return 0
-    
-    if dp[posA][posB] != -1:
-        return dp[posA][posB]
-    
-    nxt = max(posA, posB)+1
-    # print(posA, posB, nxt)
-    if not posA:
-        dp[posA][posB] = solve(nxt, posB) + get_dist([1,1], events[nxt])
-    else:
-        dp[posA][posB] = solve(nxt, posB) + get_dist(events[posA], events[nxt])
-    
-    if not posB:
-        dp[posA][posB] = min(solve(posA, nxt) + get_dist([n, n], events[nxt]), dp[posA][posB])
-    else:
-        dp[posA][posB] = min(solve(posA, nxt) + get_dist(events[posB], events[nxt]), dp[posA][posB])
 
-    return dp[posA][posB]
+    if dp[a][b] != -1:
+        return dp[a][b]
 
-def reconstruct(posA, posB):
-    if posA == m or posB == m:
+    move1 = solve(nxt, b) + dist1(a, nxt)
+    move2 = solve(a, nxt) + dist2(b, nxt)
+
+    dp[a][b] = min(move1, move2)
+    return dp[a][b]
+
+
+def trace(a, b):
+    nxt = max(a, b) + 1
+    if nxt > W:
         return
-    
-    nxt = max(posA, posB)+1   
-    
-    if not posA:
-        a = solve(nxt, posB) + get_dist([1, 1], events[nxt])
-    else:
-        a = solve(nxt, posB) + get_dist(events[posA], events[nxt])
-    
-    if not posB:
-        b = solve(posA, nxt) + get_dist([n, n], events[nxt])
-    else:
-        b = solve(posA, nxt) + get_dist(events[posB], events[nxt])
-    
-    if a > b:
-        trace.append(2)
-        reconstruct(posA, nxt)
-    else:
-        trace.append(1)
-        reconstruct(nxt, posB)
 
+    move1 = solve(nxt, b) + dist1(a, nxt)
+    move2 = solve(a, nxt) + dist2(b, nxt)
 
-n = int(input())
-m = int(input())
-events = [[]]*(m+1)
-for i in range(1, m+1):
-    events[i] = list(map(int, input().split()))
-
+    if move1 <= move2:
+        print(1)
+        trace(nxt, b)
+    else:
+        print(2)
+        trace(a, nxt)
 print(solve(0, 0))
-reconstruct(0, 0)
-for i in trace:
-    print(i)
+trace(0, 0)
