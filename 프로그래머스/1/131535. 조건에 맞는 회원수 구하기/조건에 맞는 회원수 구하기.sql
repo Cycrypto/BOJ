cb8dcd939b36295b02cb3e0f6c3e1381fa666d8a
ParkJunHa <jh01love00@gmail.com>
2026-03-26 21:25:55 +0900

    [level 1] Title: 조건에 맞는 회원수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
+++ 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
@@ -1,6 +1,4 @@
-select count(*) as users from user_info
-where 
-    age is not null
-    and extract(year from joined) = 2021
-    and age >= 20
-    and age <= 29
+select count(*) as users
+from user_info
+where date_format(joined, '%Y') = date_format('2021-00-00', '%Y')
+and age between 20 and 29
