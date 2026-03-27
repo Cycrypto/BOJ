e1ae59813c79f0c8cbf621cbad1b4852e538f8d3
ParkJunHa <jh01love00@gmail.com>
2026-03-27 16:03:36 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,7 +1,6 @@
-select member_id, member_name, gender, date_format(date_of_birth, '%Y-%m-%d')
-from member_profile
-where
-    gender = 'W' and
-    date_of_birth like '%-03-%' and
-    tlno is not null
-order by member_id asc
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') AS DATE_OF_BIRTH
+FROM MEMBER_PROFILE
+WHERE GENDER = 'W'
+  AND MONTH(DATE_OF_BIRTH) = 3
+  AND TLNO IS NOT NULL
+ORDER BY MEMBER_ID ASC;
