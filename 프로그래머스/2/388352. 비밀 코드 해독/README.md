4965d1b0f17b54b0c979b7c5b08e82f432971914
ParkJunHa <jh01love00@gmail.com>
2026-03-27 14:45:58 +0900

    [level 2] Title: 비밀 코드 해독, Time: 215.31 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/README.md
+++ 프로그래머스/2/388352. 비밀 코드 해독/README.md
@@ -1,10 +1,10 @@
 # [level 2] 비밀 코드 해독 - 388352 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388352?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388352) 
 
 ### 성능 요약
 
-메모리: 9.25 MB, 시간: 1007.67 ms
+메모리: 9.21 MB, 시간: 215.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 04일 16:25:53
+2026년 03월 27일 14:45:56
 
 ### 문제 설명
 
--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -1,13 +1,22 @@
 from itertools import combinations
 
-
 def solution(n, q, ans):
-    cnt = 0
-    for comb in combinations(range(1, n+1), 5):
-        is_ok = True
-        for x, i in enumerate(q):
-            if len(set(comb) & set(i)) != ans[x]:
-                is_ok = False
-        if is_ok:
-            cnt += 1
-    return cnt
+    res = 0
+    
+    for comb in combinations(range(1, n + 1), 5):
+        valid = True
+        
+        for d, query in enumerate(q):
+            c = 0
+            for j in range(5):
+                if comb[j] in query:
+                    c += 1
+            
+            if c != ans[d]:
+                valid = False
+                break
+        
+        if valid:
+            res += 1
+    
+    return res
