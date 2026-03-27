062555d40439330b3ca67958c227998277f988d8
ParkJunHa <jh01love00@gmail.com>
2026-03-27 11:37:16 +0900

    [level 2] Title: 힌트 스테이지, Time: 100.20 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.py
+++ 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.py
@@ -1,23 +1,31 @@
 def solution(cost, hint):
-    n = len(cost)
+    final_stage = len(cost)
+    hint_storage = [0] * (final_stage + 1)
     
-    def add_bundle(cnt, bundle):
-        nxt = cnt[:]
-        for stage_no in bundle[1:]:
-            nxt[stage_no - 1] += 1
-        return nxt
+    def dfs(stage, storage, score):
+        if stage == final_stage:
+            return score
+        
+        hint_count = min(storage[stage], len(cost[stage]) - 1)
+
+        storage = storage.copy()
+        storage[stage] = 0
+
+        score += cost[stage][hint_count]
+
+        # 마지막 스테이지는 hint가 없음
+        if stage == final_stage - 1:
+            return score
+        
+        cp_storage = storage.copy()
+        cp_score = score + hint[stage][0]
+        for h in hint[stage][1:]:
+            cp_storage[h - 1] += 1
+        
+        next_stage = stage + 1
+        return min(
+            dfs(next_stage, storage, score),
+            dfs(next_stage, cp_storage, cp_score)
+        )
     
-    def dfs(i, cnt):
-        clear = cost[i][min(cnt[i], n-1)]
-        
-        if i == n - 1:
-            return clear
-        
-        next_cnt = cnt[:]
-        next_cnt[i] = 0
-        
-        skip = clear + dfs(i + 1, next_cnt)
-        buy = clear + hint[i][0] + dfs(i + 1, add_bundle(cnt, hint[i]))
-        
-        return min(skip, buy)
-    return dfs(0, [0] * n)
+    return dfs(0, hint_storage, 0)
